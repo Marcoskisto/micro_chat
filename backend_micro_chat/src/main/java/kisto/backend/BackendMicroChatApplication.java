844b34be28dc0c7f21e2ac06f459ed51934635ad
marcoskisto <marcos@kisto.com.br>
2022-03-24 21:27:54 -0300

    Spring boot Starter

---
+++ backend_micro_chat/src/main/java/kisto/backend/BackendMicroChatApplication.java
@@ -0,0 +1,13 @@
+package kisto.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMicroChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMicroChatApplication.class, args);
+	}
+
+}
